782ec224de0e28fd84614fa727025dd1b970ced8
guandi97 <alex@micropenguin.net>
2013-12-01 11:20:21 -0500

    Update html.h

--- html.h
+++ html.h
@@ -4,11 +4,14 @@
         //path to libcurl for unix
 #endif
 
-void vinyl_get_html(char* url)
+void vinyl_get_html(char*,FILE*); //internal http-protocol
+void scratch_get_html(char*,FILE*); //external curl-lib
+
+void vinyl_get_html(char* url,FILE* stream)
 {
 
 }
-void scratch_get_html(char* url)
+void scratch_get_html(char* url,FILE* stream)
 {
 
 }
